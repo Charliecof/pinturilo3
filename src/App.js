dbfa1eacea3cf187409051d41f14cfae9fe8cdd6
Charliecof <a01328800@itesm.mx>
2022-06-20 11:23:46 -0500

    last fix url

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { ApolloProvider,InMemoryCache } from "@apollo/react-hooks";
 import { ApolloClient } from "@apollo/client";
 
 const client = new ApolloClient({
-	uri: 'http://localhost:8000/graphql/',
+	uri: 'http://194.195.219.130:8000/graphql/',
 	credentials: 'include',
 	cache: new InMemoryCache()
 })
